48e081246677905deb3a3fffe7e1dfc1e1f69bc1
Antoni Załupka <67598688+azizko1337@users.noreply.github.com>
2024-03-14 19:37:21 +0100

    Poprawione zadanie 2

---
+++ php_02_ochrona_dostepu/app/auth/views/no_permission.php
@@ -0,0 +1,25 @@
+<?php require_once dirname(__FILE__) . '/../../../config.php'; ?>
+
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Kalkulatory</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css"
+        integrity="sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Jesteś zalogowany, ale nie masz uprawnień do przeglądania tej strony.</h1>
+
+    <nav>
+        <a href="<?php print(_APP_URL) ?>">Powrot do strony glownej</a>
+    </nav>
+
+
+
+
+</body>
+
+</html>
---
+++ php_02_ochrona_dostepu/app/views/loan_view.php
@@ -0,0 +1,51 @@
+<?php require_once dirname(__FILE__) . '/../auth/controllers/secured_admin.php'; ?>
+<?php require_once dirname(__FILE__) . '/../../config.php'; ?>
+
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Kalkulator</title>
+</head>
+
+<body>
+    <nav>
+        <a href="<?php print(_APP_URL); ?>/app/views/calc_view.php">Kalkulator liczbowy</a>
+        <a href="<?php print(_APP_URL); ?>/app/views/loan_view.php">Kalkulator kredytowy</a>
+        <a href="<?php print(_APP_URL); ?>/app/auth/controllers/logout.php">Wyloguj</a>
+    </nav>
+
+    <h1>Kalkulator kredytowy</h1>
+
+    <form action="<?php print(_APP_URL); ?>/app/controllers/loan.php" method="post"
+        style="display:grid; grid-template-columns: 200px 200px;">
+        <label for="amount">Kwota kredytu:</label>
+        <input id="amount" name="amount" type="number" value="<?php print($amount ?? "") ?>">
+        <label for="years">Dlugosc (w latach):</label>
+        <input id="years" name="years" type="number" value="<?php print($years ?? "") ?>">
+        <label for="percentage">Oprocentowanie:</label>
+        <input id="percentage" name="percentage" type="number" value="<?php print($percentage ?? "") ?>">
+        <input value="Oblicz" type="submit">
+    </form>
+
+    <?php
+    if (isset($messages)) {
+        if (count($messages) > 0) {
+            echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
+            foreach ($messages as $key => $msg) {
+                echo '<li>' . $msg . '</li>';
+            }
+            echo '</ol>';
+        }
+    }
+    ?>
+
+    <?php if (isset($result)) { ?>
+        <div style="margin: 20px; padding: 10px; border-radius: 5px; background-color: #ff0; width:300px;">
+            <?php echo 'Wynik: ' . $result . 'PLN miesiecznie.'; ?>
+        </div>
+    <?php } ?>
+</body>
+
+</html>
